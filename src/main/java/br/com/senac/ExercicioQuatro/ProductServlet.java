d69404307e3df60f92904516456a4c1208070f92
Jackeline <jackelinesouza03@gmail.com>
2026-06-25 21:45:34 -0300

    Criado arquivo ProductServlet

--- src/main/java/br/com/senac/ExercicioQuatro/ProductServlet.java
+++ src/main/java/br/com/senac/ExercicioQuatro/ProductServlet.java
@@ -1,4 +1,27 @@
 package br.com.senac.ExercicioQuatro;
 
-public class ProductServlet {
+import br.com.senac.HelloServlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/produto/*")
+public class ProductServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+
+        String pathInfo = req.getPathInfo();
+
+        if (pathInfo != null && pathInfo.length() > 1) {
+            resp.getWriter().println("<h1>Você está vendo os detalhes do produto: " + pathInfo.substring(1) + "</h1>");
+        } else {
+            resp.getWriter().println("<h1>Produto não informado</h1>");
+        }
+    }
 }
